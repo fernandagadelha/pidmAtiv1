b2dbeeb92cb21ff7d2d975f3a89100fc5f28d684
Fernanda <fegadelhasouza@gmail.com>
2020-12-05 15:29:59 -0300

    atualizando o App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Cabecalho from './Cabecalho';
 import Corpo from './Corpo';
 import Disciplina from './Disciplina';
@@ -10,14 +11,27 @@ export default class App extends React.Component {
   
   render(){
     return(
-      <View classname="App">
-        <Cabecalho nomealuno="Bojack" cursoaluno="Design Digital"></Cabecalho>
-        <Corpo></Corpo>
-        <Disciplina disciplina="PIDM"></Disciplina>
+      <View style={styles.container}>
+        <StatusBar style="auto"/>
+        <Cabecalho nomealuno="Bojack" cursoaluno="Design Digital"/>
+        <Corpo/>
+        <Disciplina disciplina="PIDM"/>
+        <Disciplina disciplina="Design e Inovação"/>
+        <Disciplina disciplina="Projeto IV"/>
+        <Disciplina disciplina="Concepção e Desenvolvimento de Produto"/>
 
       </View>
       
     )
   }
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
   
